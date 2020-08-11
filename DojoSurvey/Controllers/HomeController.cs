a2273abfa79637ed6a2fb295734f35cd481b464d
Gary <66178774+gen8787@users.noreply.github.com>
2020-08-10 23:05:00 -0700

    DojoSurvey

---
+++ DojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+// v~~ V I E W S ~~v //
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+//
+        [HttpPost("/FormPost")]
+        public IActionResult FormPost(SurveyResults newSurveyResult)
+        {
+            return View("result", newSurveyResult);
+        }
+//
+        [HttpGet("result")]
+        public ViewResult Result()
+        {
+            return View("Result");
+        }
+//<~~ E N D   O F   C O N T R O L L E R ~~> //
+
+// v~~ O T H E R ~~v //
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
